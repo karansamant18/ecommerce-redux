6b468b926b0c649df27e710096009d4947da7b7c
karansamant <141988116+karansamant@users.noreply.github.com>
2024-06-24 12:55:57 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './containers/header';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductListing from './containers/productListing';
+import ProductDetail from './containers/productDetail';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<ProductListing />} />
+            <Route path="/product/:productId" element={<ProductDetail />} />
+            <Route path="*" element={<div>404 Not Found!</div>} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/containers/productDetail.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios';
+import { selectedProducts } from '../redux/actions/productActions';
+import { useDispatch, useSelector } from 'react-redux';
+
+const ProductDetail = () => {
+    const product = useSelector((state) => state.product);
+    const { productId } = useParams();
+    const dispatch = useDispatch();
+    console.log(product);
+
+
+    const fetchProductDetail = async () => {
+        const response = await axios
+            .get(`https://fakestoreapi.com/products/${productId}`)
+            .catch((err) => {
+                console.log("Err", err);
+            });
+        dispatch(selectedProducts(response.data));
+    }
+
+    useEffect(() => {
+        if (productId && productId !== "") fetchProductDetail();
+    }, [productId]);
+
+    return (
+        <div>
+            <h1>ProductDetail</h1>
+
+        </div>
+    )
+}
+
+export default ProductDetail
